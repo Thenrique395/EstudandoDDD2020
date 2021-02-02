35144b22842720ff20ac5254476bd76c8ffe00d0
Tiago <thenrique395@gmail.com>
2021-02-02 15:18:35 -0300

    Implementação que validações da entidade  product

--- Domain/interfaces/interfaceServices/IServiceProduct.cs
+++ Domain/interfaces/interfaceServices/IServiceProduct.cs
@@ -1,6 +1,12 @@
-﻿namespace Domain.interfaces.interfaceServices
+﻿using Entities.Entities;
+using System.Threading.Tasks;
+
+namespace Domain.interfaces.interfaceServices
 {
     public interface IServiceProduct
     {
+        Task UpdateProduct(Product product);
+        Task AddProduct(Product product);
+
     }
 }
